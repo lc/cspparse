611660a4880e2e6ed5d46a69dc17dfaa33ae56b8
Riley Johnson <rileyjohnson99@hotmail.com>
2018-10-10 21:39:04 -0500

    Finished changes, all requests are done with stdlib

--- cspparse.go
+++ cspparse.go
@@ -14,8 +14,6 @@ Contributor(s):
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -23,16 +21,8 @@ import (
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/imroc/req"
 	"github.com/pkg/errors"
 )
-
-// create a struct (a collection of fields) to
-// "unravel" Google's API JSON response into
-type cspStatus struct {
-	Status string `json:"status"`
-	Csp    string `json:"csp"`
-}
 
 // define a map of lists (with type string)
 // globally so any function can access it
@@ -51,17 +41,41 @@ func main() {
 	// if user passes a command line argument (the domain  / url to check)
 	if len(os.Args) > 1 {
 
-		// set the variable to the first argument passed
+		// Set the variable to the first argument passed
 		domain := os.Args[1]
 
+		// Validate the given URL
+		_, err := url.ParseRequestURI(domain)
+
+		if err != nil {
+			fmt.Println("Domain must be a valid URL")
+			os.Exit(1)
+		}
+
 		// pass the domains to our functions
-		getCSPApi(domain)
-		getCSPHtml(domain)
+		err = getCSPApi(domain)
 
-		// dump the map into pretty json
-		bytes, _ := json.MarshalIndent(cspObject, "", "    ")
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "could not get CSP information from Google API"))
+			os.Exit(1)
+		}
 
-		// print it to the user
+		err = getCSPHtml(domain)
+
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "could not get CSP information from domain HTML"))
+			os.Exit(1)
+		}
+
+		// Pump the map into pretty printed json
+		bytes, err := json.MarshalIndent(cspObject, "", "    ")
+
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "error formatting output JSON"))
+			os.Exit(1)
+		}
+
+		// Print it to the user
 		fmt.Println(string(bytes))
 	} else {
 		// if no domain or url is passed show the usage to the user.
@@ -69,114 +83,89 @@ func main() {
 	}
 }
 
-func getCSPApi(domain string) (string, error) {
-	// set the parameters of the POST request
-	// to url=<domain>
-	client := &http.Client{Timeout: 2 * time.Second}
-
-	// params := req.Param{
-	// 	"url": domain,
-	// }
-
+func getCSPApi(domain string) error {
+	// Need to set a timeout because the default request client has none
+	client := &http.Client{Timeout: 3 * time.Second}
 	requestURL := fmt.Sprintf("https://csp-evaluator.withgoogle.com/getCSP?url=%s", url.QueryEscape(domain))
 
-	// make the request
-	// req.SetTimeout(2 * 1000 * 1000 * 1000)
-
-	// r, err := req.Post("https://csp-evaluator.withgoogle.com/getCSP", params)
-	resp, err := client.Get(requestURL)
+	// Make a POST request to the endpoint, no content-type nor request body needed
+	resp, err := client.Post(requestURL, "", nil)
 
 	if err != nil {
-		return "", errors.Wrap(err, "error making request")
+		return errors.Wrap(err, "error making request to csp-evaluator")
 	}
 
-	// Defer closing the connection
+	// Defer closing the connection until after we leave the function
 	defer resp.Body.Close()
 
-	var c cspStatus
+	// create an anonymous struct to marshall the JSON from Google's API
+	cspStatus := struct {
+		Status string `json:"status"`
+		Csp    string `json:"csp"`
+	}{}
 
-	// JSON Decode Google's CSP response from JSON into the struct
-	err = json.NewDecoder(resp.Body).Decode(&c)
+	// Marshall Google's CSP response JSON into the struct
+	err = json.NewDecoder(resp.Body).Decode(&cspStatus)
 
 	if err != nil {
-		return "", errors.Wrap(err, "error decoding response JSON")
+		return errors.Wrap(err, "error decoding response JSON")
 	}
 
-	// If Google gave us status:"ok" there is a CSP.
-	if c.Status == "ok" {
-		// Rules are ';' delimited, split the CSP by ';' into rules
-		cspResult := strings.Split(c.Csp, ";")
+	// If Google gave did not give us 'status: "ok"' there is not a CSP
+	if cspStatus.Status != "ok" {
+		return errors.New("no CSP for the domain given")
+	}
 
-		for _, result := range cspResult {
-			if result != "" {
-				// Split the rule by the first space to get valid JSON
-				rules := strings.Split(result, " ")
+	// Rules are ';' delimited, split the CSP by ';' into rules
+	cspResult := strings.Split(cspStatus.Csp, ";")
 
-				// ex: default-src * data: blob:; -> "default-src": ["*","data:","blob:"],
-				cspObject[rules[0]] = append(make([]string, 0), rules[1:]...)
-			}
+	for _, result := range cspResult {
+		if result != "" {
+			// Split the rule by the first space to get valid JSON
+			rules := strings.Split(result, " ")
+
+			// ex: default-src * data: blob:; -> "default-src": ["*","data:","blob:"],
+			cspObject[rules[0]] = append(make([]string, 0), rules[1:]...)
 		}
-
 	}
 
-	return "", errors.New("no CSP for the domain given")
+	return nil
 }
-func getCSPHtml(domain string) string {
-	// set the variable to the response body
-	// from the function request()
-	htmlCode := request(domain)
-	// use goquery to parse the HTML.
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader((htmlCode)))
+func getCSPHtml(domain string) error { // Need to set a timeout because the default request client has none
+	client := &http.Client{Timeout: 3 * time.Second}
+
+	resp, err := client.Get(domain)
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
 	if err != nil {
-		log.Fatal(err)
+		return errors.Wrap(err, "could not parse HTML from domain")
 	}
-	// find all <meta> tags to see if there is a CSP.
+
+	// Find all <meta> tags to see if there is a CSP
 	doc.Find("meta").Each(func(i int, s *goquery.Selection) {
-		// if we find a <meta> tag with the
-		// attribute http-equiv set to "Content-Security-Policy"
+		// Check if <meta> tag has the attribute http-equiv set to "Content-Security-Policy"
 		if name, _ := s.Attr("http-equiv"); name == "Content-Security-Policy" {
-			// grab the CSP rule from the "content=" attribute
+			// Grab the CSP rule from the "content" attribute
 			description, _ := s.Attr("content")
-			// delete spaces trailing after semicolons before splitting
+
+			// Delete spaces trailing after semicolons before splitting
 			delSpace := strings.Replace(description, "; ", ";", -1)
-			// split the CSP into rules by the ; separator
+
+			// Split the CSP into rules by the ; separator
 			cspResult := strings.Split(delSpace, ";")
 
 			for _, result := range cspResult {
 				if result != "" {
-					// split the rule by the first space to get valid JSON
+					// Split the rule by the first space to get valid JSON
 					rules := strings.Split(result, " ")
-					// ex: default-src * data: blob:; -> "default-src": ["*","data:","blob:"],
+
+					// Ex: default-src * data: blob:; -> "default-src": ["*","data:","blob:"],
 					cspObject[rules[0]] = append(make([]string, 0), rules[1:]...)
 				}
 			}
 		}
 	})
-	return ""
-}
-func request(url string) string {
-	// make a GET request to the target domain / URL
-	if url != "" {
-		req.SetTimeout(2 * 1000 * 1000 * 1000)
-		r, err := req.Get(url)
 
-		if err != nil {
-			fmt.Printf("Error making request:\n%s\n", err)
-			os.Exit(1)
-		}
-
-		resp := r.Response()
-		// close the connection
-		defer resp.Body.Close()
-		// read the response body from the target
-		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
-		if err2 != nil {
-			fmt.Printf("Error: %s\n", err2)
-		} else {
-			bodyString := string(bodyBytes)
-			// return the response body
-			return bodyString
-		}
-	}
-	return ""
+	return nil
 }
