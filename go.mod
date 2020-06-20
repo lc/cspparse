06cadabaea20c47f066bc5a54cff48af7229c650
lc <corben@lynxsecurity.io>
2020-06-20 10:41:56 -0500

    Move to go modules

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/lc/cspparse
+
+go 1.14
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1
+	github.com/pkg/errors v0.9.1
+)
--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-go get -u github.com/pkg/errors
-go get -u github.com/PuerkitoBio/goquery
-go build cspparse.go
-mv cspparse $GOPATH/bin
